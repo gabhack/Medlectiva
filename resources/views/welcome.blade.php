ffe57327fe19840274e2816c963d1b275ac662e1
spotfinder-dev <pbl.mendez@gmail.com>
2023-08-16 01:29:58 -0400

    banner home changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,28 +16,28 @@
         <main class="py-5">
             <section class="hero-banner bg-gray-100 py-10">
                 <div class="w-full">
-                    <div class="container mx-auto flex">
+                    <div class="container mx-auto flex flex-col md:flex-row">
 
                         <!-- Div del texto a la izquierda -->
-                        <div class="left flex-1 p-5 flex flex-col justify-center">
-                            <h1 class="text-4xl font-bold mb-4">Inicia una rotación médica de excelencia</h1>
+                        <div class="left flex-1 p-5 flex flex-col justify-center mb-4 md:mb-0">
+                            <h1 class="text-3xl md:text-4xl font-bold mb-4">Inicia una rotación médica de excelencia
+                            </h1>
                             <p class="mb-6 text-lg">Sumérgete en la experiencia clínica con programas de rotación en
                                 hospitales y clínicas reconocidas a nivel mundial. Aprende de los mejores y construye un
                                 sólido cimiento para tu carrera médica.</p>
                             <a href="/rotations"
-                                class="btn btn-primary text-sm inline-block bg-blue-500 text-white px-8 py-3 rounded hover:bg-blue-600 w-100 w-30 w-50">Explora
+                                class="btn btn-primary text-sm inline-block bg-blue-500 text-white px-6 py-2 md:px-8 md:py-3 rounded hover:bg-blue-600 w-full md:w-auto">Explora
                                 todos los programas de rotación</a>
                         </div>
-
 
                         <!-- Div de la imagen a la derecha -->
                         <div class="right flex-1 p-5">
                             <img src="https://i.postimg.cc/3JqfrcLD/portada.png" alt="Imagen de un medico"
                                 class="max-w-full h-full">
                         </div>
-
                     </div>
             </section>
+
 
 
             <div class="container mx-auto">
