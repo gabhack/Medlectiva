eae48b4d15202080f10735d6389a873af77329f6
spotfinder-dev <pbl.mendez@gmail.com>
2023-08-25 11:11:02 -0400

    Cambios en header y logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menú MedLectiva</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
